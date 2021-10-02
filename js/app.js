6521deaa6d2ff0a561f6eefb3867857417534d70
Marni Hager <mlh6118@gmail.com>
2021-10-02 14:18:48 -0700

    Added number guessing game.

--- js/app.js
+++ js/app.js
@@ -63,3 +63,37 @@ else{
   // console.log('That is incorrect.');
   alert('That is incorrect, ' + userName + '.');
 }
+
+// Create a guessing game that prompts the user for Marni's favorite number.  Give the user a total of four (4) guesses.  Let the user know if the guesses are too high, too low, or correct.  After all guesses are used up and still incorrect, let the user know what the correct answer is.
+
+// Declare variables for number guessing game.
+let numberGuessed = 0;
+let numberOfGuesses = 4;
+let correctAnswer = Math.floor(Math.random()*101); // Set up a random number between 0 and 100.
+
+// Inform the user of the game.
+alert('Let\'s play a guessing game.  You will get four (4) chances to guess a computer generated number between 0 and 100.');
+
+// Prompt user for guess.
+numberGuessed = prompt('Please enter your guess for the computer generated number.');
+
+while(parseInt(numberGuessed) !== correctAnswer && (numberOfGuesses - 1) > 0){
+  numberOfGuesses --;
+  if (parseInt(numberGuessed) < correctAnswer){
+    numberGuessed = prompt('You have incorrectly guessed the number.  The actual number is higher.  Please guess again.');
+  }
+  else if(parseInt(numberGuessed) > correctAnswer){
+    numberGuessed = prompt('You have incorrectly guessed the number.  The actual number is lower.  Please guess again.');
+  }
+  else{
+    numberGuessed = prompt('You have not entered a valid number.  Please guess again.');
+  }
+}
+
+if(parseInt(numberGuessed) === correctAnswer){
+  alert('You have guessed the computer generated number!');
+}
+else{
+  alert('The computer generated number was ' + correctAnswer);
+}
+
