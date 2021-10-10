3def02f9a6c534cc7d0f83f8820399b4b1984a11
Marni Hager <mlh6118@gmail.com>
2021-10-09 18:12:00 -0700

    Cleaned up code.

--- js/app.js
+++ js/app.js
@@ -8,67 +8,52 @@ let userName = prompt('Please enter your first name.');
 alert(userName + ', you will now be asked five questions.');
 
 let raisedIn = prompt('Marni was raised in San Francisco, CA? (Yes/No)').toLowerCase();
-// console.log(raisedIn);
 
 if(raisedIn === "no" || raisedIn === "n"){
-  // console.log('You have answered correctly.');
   alert('You have answered correctly.');
   totalCorrect = totalCorrect + 1;
 }
 else{
-  // console.log('That is incorrect.');
   alert('That is incorrect.');
 }
 
 let degrees = prompt('Marni has two BS degrees? (Yes/No)').toLowerCase();
-// console.log(degrees);
 
 if(degrees === "no" || degrees === "n"){
-  // console.log('You have answered correctly.');
   alert('You have answered correctly.');
   totalCorrect = totalCorrect + 1;
 }
 else{
-  // console.log('That is incorrect.');
   alert('That is incorrect.');
 }
 
 let computers = prompt('Marni has experience with 4 types of computers? (Yes/No)').toLowerCase();
-// console.log(computers);
 
 if(computers === "yes" || computers === "y"){
-  // console.log('You have answered correctly.');
   alert('You have answered correctly.');
   totalCorrect = totalCorrect + 1;
 }
 else{
-  // console.log('That is incorrect.');
   alert('That is incorrect.');
 }
 
 let jobs = prompt('Marni has experience as a teacher? (Yes/No)').toLowerCase();
-// console.log(jobs);
 
 if(jobs === "yes" || jobs === "y"){
-  // console.log('You have answered correctly.');
   alert('You have answered correctly.');
   totalCorrect = totalCorrect + 1;
 }
 else{
-  // console.log('That is incorrect.');
   alert('That is incorrect.');
 }
 
 let puzzle = prompt('Marni views programming like a puzzle? (Yes/No)').toLowerCase();
-// console.log(puzzle);
 
 if(puzzle === "yes" || puzzle === "y"){
-  // console.log('You have answered correctly.');
   alert('You have answered correctly, ' + userName + '.');
   totalCorrect = totalCorrect + 1;
 }
 else{
-  // console.log('That is incorrect.');
   alert('That is incorrect, ' + userName + '.');
 }
 
@@ -129,11 +114,8 @@ alert('Let\'s play a new game.  Try to guess one of my favorite activities using
 // Prompt user for guess.
 activityGuessed = prompt('Please enter one or two words that you think are a favorite activity of Marni\'s.');
 
-// activityGuessed = activityGuessed.toLowerCase();
-
 for(let j = (guessesRemaining - 1); j > 0; j --){ // Must use -1 to account for the initial guess above.
   for(let i = 0; i < favActivities.length; i++){
-    // alert(favActivities[i]);
     if (activityGuessed.toLowerCase() === favActivities[i]){
       alert('You have guessed a favorite activity of Marni\'s correctly!');
       answeredCorrectly = 'Yes';
